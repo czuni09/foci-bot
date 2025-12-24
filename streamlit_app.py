404546c47a118b2295d630e1826e35ab2ad30ceb
czuni09 <ddani3839@gmail.com>
2025-12-24 13:13:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,12 @@
 import streamlit as st
-from ultimate_football_bot import ultimate_football_bot, TeamStats
+import ultimate_football_bot as bot
 
-st.set_page_config(page_title="Foci Elemző Bot", layout="wide")
-st.title("⚽ czunidaniel9 Elemző Központ")
+st.title("⚽ czunidaniel9 Foci Bot")
+st.write("A rendszer minden nap 10:00-kor küld e-mailt.")
 
-st.info("A rendszer minden nap 10:00-kor és meccs előtt 1 órával küldi az elemzést e-mailben.")
-
-# Példa egy elemzésre a felületen
-if st.button('Mai kiemelt meccs elemzése'):
-    # Példa adatok (Arsenal vs Crystal Palace)
-    hazai = TeamStats("Arsenal", 85, 80, [1, 1, 0, 1], injuries=["Gabriel Jesus"])
-    vendeg = TeamStats("Crystal Palace", 70, 75, [0, 1, -1], intl_absences=["Jordan Ayew"])
-    
-    # Lefuttatjuk a botot
-    ultimate_football_bot(hazai, vendeg, "London", "Anthony Taylor", "Premier League", 1.48, 60, 3.1)
-    st.success("Az elemzés elkészült! Részletek a konzolon és hamarosan az e-mailedben.")
+if st.button("Teszt elemzés indítása most"):
+    st.write("Elemzés és e-mail küldés folyamatban...")
+    h = bot.TeamStats("Arsenal")
+    v = bot.TeamStats("Crystal Palace")
+    bot.ultimate_football_bot(h, v, "London", "PL", 1.5, 80)
+    st.success("Kész! Ellenőrizd az e-mailedet!")
